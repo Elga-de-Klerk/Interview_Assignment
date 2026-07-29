4fdab0f6a2f24d41f2c51c143e919aa28ed78bbc
Elga <elga.de.klerk@gmail.com>
2026-07-29 17:26:21 +0200

    Base endpoint and swagger

---
+++ src/Documents/Documents.Api/Controllers/DocumentsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Documents.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DocumentsController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult MutateDocument()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
