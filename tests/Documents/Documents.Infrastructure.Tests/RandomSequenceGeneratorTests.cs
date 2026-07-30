26c6fc826c72128028882dfc0e22284299844c4a
Elga <elga.de.klerk@gmail.com>
2026-07-30 18:43:51 +0200

    RandomSequenceGeneratorTests

---
+++ tests/Documents/Documents.Infrastructure.Tests/RandomSequenceGeneratorTests.cs
@@ -0,0 +1,35 @@
+﻿using Documents.Infrastructure;
+
+namespace Document.Infrastructure.Tests
+{
+    public class RandomSequenceGeneratorTests
+    {
+        [Fact]
+        public void Generate_WhenNoLengthDefined_ShouldReturnDefault8CharacterRandomSequence()
+        {
+            // Arrange
+            var generator = new RandomSequenceGenerator();
+
+            // Act
+            var result = generator.Generate();
+
+            // Assert
+            Assert.Equal(8, result.Length);
+            Assert.Matches("^[A-Za-z0-9]+$", result);
+        }
+
+        [Fact]
+        public void Generate_WhenLengthDefined_ShouldReturnRandomSequenceOfDefinedLength()
+        {
+            // Arrange
+            var generator = new RandomSequenceGenerator();
+
+            // Act
+            var result = generator.Generate(24);
+
+            // Assert
+            Assert.Equal(24, result.Length);
+            Assert.Matches("^[A-Za-z0-9]+$", result);
+        }
+    }
+}
